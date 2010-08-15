20e682508000e062407f71ae1eea14c2fc47c8c9
ivanwyc <ivanwyc@85774f28-1b3f-11df-8198-7301c629e857>
2010-08-15 17:16:24 +0000

    again...

--- trunk/src/fastweb/fastweb.go
+++ trunk/src/fastweb/fastweb.go
@@ -911,7 +911,7 @@ func (a *Application) Handle(r *fastcgi.Request) bool {
 func NewApplication() *Application {
 	return &Application{
 		controllerMap:     make(map[string]*controllerInfo),
-		defaultController: "default",
+		defaultController: "Default",
 	}
 }
 
